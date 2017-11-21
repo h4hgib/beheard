3f865a397d3a6d6def561d749e3a548ba1a5eaac
Charris <c@harrischris.com>
2017-11-21 12:14:49 +0100

    [Build] Hot reloading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './components/App'
 
-import App from './components/App';
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+};
 
-render(
-  <App />,
-  document.querySelector('#root')
-);
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/App', () => { render(App) })
+};
