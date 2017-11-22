67741da50bca79ba0a95f6d6afbe09f52ad109a5
Graeme Fulton <graemefulton89@gmail.com>
2017-11-22 18:07:08 +0100

    landing page description

--- src/pages/PrelaunchPage.js
+++ src/pages/PrelaunchPage.js
@@ -15,6 +15,13 @@ const PrelaunchPage = function PrelaunchPage(props) {
     fontWeight:200, margin:'0px',
     marginTop:'30px', marginBottom:'25px'
   }
+  var flexContainerStyle = {
+    display:'flex',
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'flex-start',
+    alignItems:'flex-start'
+  }
 
   return (
     <div>
@@ -25,6 +32,31 @@ const PrelaunchPage = function PrelaunchPage(props) {
                 <div style={tagLineStyle}>
                   <span id="learnmore"/>
                   <h1 style={tagLineFontStyle}>Why Be Heard?</h1>
+                  <Grid item xs={6} style={{textAlign:'center', margin:'0 auto'}}>
+                    <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum </p>
+                  </Grid>
+                    <Grid item xs={12} style={flexContainerStyle}>
+                        <Grid xs={4} item>
+                          <Paper className='item'>
+                             Hola
+                          </Paper>
+                        </Grid>
+                        <Grid xs={6} item>
+                          <Paper className='item'>
+                            que
+                          </Paper>
+                      </Grid>
+                      <Grid container justify="center" spacing={Number(16)}>
+                      {[0, 1, 2, 3, 4, 5].map(value => (
+                        <Grid xs={4} key={value} item>
+                          <a href="/article1">
+                            <Paper style={{background:'#fff', backgroundImage:'url("/images/dummy/00'+value+'.png")',
+                             height: 300, backgroundSize:'cover', backgroundPosition:'center',backgroundPositionY:'-40px',backgroundRepeat:'no-repeat', borderRadius:'3px'}}/>
+                          </a>
+                        </Grid>
+                      ))}
+                      </Grid>
+                    </Grid>
                 </div>
               </div>
             </Grid>
